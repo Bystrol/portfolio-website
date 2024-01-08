5a8c8b8a0256fdfa039952c51b8698d6dc4838d8
Michał <michalbystryk14@wp.pl>
2024-01-08 22:20:24 +0100

    feat: add LargeVector component

---
+++ src/components/svg/LargeVector.tsx
@@ -0,0 +1,32 @@
+export default function LargeVector() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1920"
+      height="368"
+      viewBox="0 0 1920 368"
+      fill="none"
+      className="w-full hidden lg:block mt-[20vh]"
+    >
+      <path
+        d="M-149 201.615C-100.444 49.9119 452.771 -133.243 654.248 154.991C808.932 376.282 430.892 438.912 396.21 264.245C353.824 50.7856 819.561 -48.5138 1192.52 35.2983C1539.35 113.238 1796.69 220.812 2036 357.206"
+        stroke="url(#paint0_linear_378_97)"
+        strokeWidth="6"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_378_97"
+          x1="95.1651"
+          y1="-47.1039"
+          x2="1859.81"
+          y2="400.421"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.157462" stopColor="#5035DA" />
+          <stop offset="0.538552" stopColor="#2960F8" />
+          <stop offset="0.893594" stopColor="#111111" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
