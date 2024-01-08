a2903f9b3e954bc6aace4f0800a60c895138c4de
Michał <michalbystryk14@wp.pl>
2024-01-08 20:24:03 +0100

    feat: add SmallVector component

---
+++ src/components/svg/SmallVector.tsx
@@ -0,0 +1,32 @@
+export default function SmallVector() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="390"
+      height="141"
+      viewBox="0 0 390 141"
+      fill="none"
+      className="w-full sm:hidden mt-[26svh]"
+    >
+      <path
+        d="M-194 76.9084C-175.622 19.693 33.764 -49.3845 110.021 59.3238C168.567 142.785 25.4832 166.406 12.3562 100.529C-3.68615 20.0225 172.59 -17.4286 313.752 14.1814C445.022 43.5765 542.424 84.1487 633 135.59"
+        stroke="url(#paint0_linear_105_1662)"
+        strokeWidth="3"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_105_1662"
+          x1="-101.586"
+          y1="-16.8969"
+          x2="566.026"
+          y2="153.014"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset="0.157462" stopColor="#5035DA" />
+          <stop offset="0.538552" stopColor="#2960F8" />
+          <stop offset="0.893594" stopColor="#111111" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
