4b0d69434aeb83f58b25298b63f67652bb79fcbd
Michał <michalbystryk14@wp.pl>
2024-01-11 21:07:31 +0100

    feat: add CodeIcon component

---
+++ src/components/atoms/CodeIcon.tsx
@@ -0,0 +1,44 @@
+export default function CodeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="28"
+      height="29"
+      viewBox="0 0 28 29"
+      fill="none"
+    >
+      <rect
+        y="0.5"
+        width="28"
+        height="28"
+        rx="14"
+        fill="url(#paint0_linear_110_1708)"
+      />
+      <path
+        d="M7.2002 8.5L13.6002 14.1L7.2002 19.7"
+        stroke="white"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      />
+      <path
+        d="M15.2002 19.7H21.6002"
+        stroke="white"
+        strokeWidth="1.2"
+        strokeLinecap="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_110_1708"
+          x1="-1.66486"
+          y1="0.499996"
+          x2="32.0821"
+          y2="2.24861"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#5035DA" />
+          <stop offset="1" stopColor="#2960F8" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
