ca636fa327265be47b94e0ccf35e1c65ddd8e7a1
Michał <michalbystryk14@wp.pl>
2024-01-12 15:54:00 +0100

    feat: add ArrowTopRight component

---
+++ src/components/atoms/ArrowTopRight.tsx
@@ -0,0 +1,20 @@
+export default function ArrowTopRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      className="sm:w-[30px] sm:h-[30px]"
+    >
+      <path
+        d="M5.87521 6.62723L14.1248 6.62723M14.1248 6.62723L14.1248 14.8768M14.1248 6.62723L5.87521 14.8768"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
