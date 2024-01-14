8684c2a4c61e82ddabf393636274cecb1d10f874
Michał <michalbystryk14@wp.pl>
2024-01-14 21:28:04 +0100

    feat: add translation type declaration file

---
+++ src/types/translation.d.ts
@@ -0,0 +1,33 @@
+export type Translation = {
+  navigation: string[]
+  hero: {
+    heading: string
+    paragraph: string
+    button: string
+    availability_first: string
+    availability_second: string
+  }
+  about: {
+    heading_first: string
+    paragraph_first: string
+    paragraph_second: string
+    heading_second: string
+  }
+  offer: {
+    heading: string
+    paragraph: string
+    cards: { title: string; content: string }[]
+  }
+  projects: {
+    heading: string
+    cards: { title: string; content: string }[]
+    button: string
+  }
+  contact: {
+    heading: string
+    paragraph: string
+    form: { label: string; placeholder: string; errorMessage: string }[]
+    button: string
+    successMessage: string
+  }
+}
