1d71b96b1c1fbefb8c9e20f005324b71d0e77e9c
Michał <michalbystryk14@wp.pl>
2024-01-15 19:26:46 +0100

    style: change font size

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer({ translation }: FooterProps) {
   return (
     <footer className="w-full px-[30px] sm:px-[60px] lg:px-[120px] py-[10px] sm:py-[30px]">
       <p
-        className={`${unbounded.className} text-[10px] sm:text-[14px] text-center`}
+        className={`${unbounded.className} text-[9px] sm:text-[14px] text-center`}
       >
         © Michał Bystryk 2024. {translation.footer.paragraph}
       </p>
