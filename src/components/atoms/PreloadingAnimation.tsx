4f30eaaaf8eb1fe7c3c71619f075a2f1e4c5471a
Michał <michalbystryk14@wp.pl>
2024-01-16 13:54:33 +0100

    feat: add PreloadingAnimation component

---
+++ src/components/atoms/PreloadingAnimation.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import Lottie from 'react-lottie'
+import mobileRibbonAnimation from '../../lotties/ribbon-mobile.json'
+import desktopRibbonAnimation from '../../lotties/ribbon-desktop.json'
+
+export default function PreloadingAnimation() {
+  const [isMobileDevice, setIsMobileDevice] = useState<boolean>()
+
+  useEffect(() => {
+    const isMobile =
+      /Mobi|Android|webOS|iPhone|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    setIsMobileDevice(isMobile)
+  }, [])
+
+  const defaultLottieOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: isMobileDevice
+      ? mobileRibbonAnimation
+      : desktopRibbonAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
+
+  return (
+    <div className="w-full absolute mt-[26svh] lg:mt-[20vh]">
+      <Lottie options={defaultLottieOptions} />
+    </div>
+  )
+}
