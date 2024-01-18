1ead2b2faeaec37df58710a90d23b16d1924355b
Michał <michalbystryk14@wp.pl>
2024-01-18 23:19:01 +0100

    feat: translate icon on button hover

--- src/components/molecules/CtaButton.tsx
+++ src/components/molecules/CtaButton.tsx
@@ -13,13 +13,17 @@ export default function CtaButton({
 }: CtaButtonProps) {
   return (
     <button
-      className={`flex justify-center items-center gap-[10px] mt-[10px] px-[20px] py-[8px] w-max rounded-full border border-[#2960F8] bg-gradient-to-r from-[#5035DA] to-[#2960F8] sm:hover:drop-shadow-blue text-[14px] sm:text-[18px] lg:text-[20px] transition-all duration-200 ${
+      className={`group flex justify-center items-center gap-[10px] mt-[10px] px-[20px] py-[8px] w-max rounded-full border border-[#2960F8] bg-gradient-to-r from-[#5035DA] to-[#2960F8] sm:hover:drop-shadow-blue text-[14px] sm:text-[18px] lg:text-[20px] transition-all duration-200 ${
         isAnimated && 'animate-bounce'
       }`}
       onClick={handleClick}
     >
       {text}
-      {Icon && <Icon />}
+      {Icon && (
+        <div className="sm:group-hover:translate-y-[3px] transition-all">
+          <Icon />
+        </div>
+      )}
     </button>
   )
 }
