a058fb67ce76d1f8ffdf00936b52061ec45745c7
Michał <michalbystryk14@wp.pl>
2024-01-21 19:24:50 +0100

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { Toaster } from 'react-hot-toast'
 import type { Viewport } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -45,6 +46,7 @@ export default function RootLayout({
         <Toaster position="bottom-center" toastOptions={{ duration: 3000 }} />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
