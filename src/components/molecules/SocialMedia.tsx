ff18fcc742a92de427c4a5034c45046f2950c15b
Michał <michalbystryk14@wp.pl>
2024-01-21 12:05:50 +0100

    refactor: replace CircularButton component with a Link

--- src/components/molecules/SocialMedia.tsx
+++ src/components/molecules/SocialMedia.tsx
@@ -1,22 +1,32 @@
-import { useRouter } from 'next/navigation'
-import CircularButton from '@/components/molecules/CircularButton'
+import Link from 'next/link'
 import GithubLogo from '@/components/atoms/GithubLogo'
 import LinkedinLogo from '@/components/atoms/LinkedinLogo'
 import { githubLink, linkedinLink } from '@/utils/constants/socialMediaLinks'
 
 export default function SocialMedia() {
-  const router = useRouter()
+  const socialMediaData = [
+    {
+      link: githubLink,
+      icon: GithubLogo
+    },
+    {
+      link: linkedinLink,
+      icon: LinkedinLogo
+    }
+  ]
 
   return (
     <div className="fixed bottom-[30px] right-[10px] sm:right-[30px] lg:right-[60px] flex flex-col gap-[10px] animate-slideLeftMobile lg:animate-slideLeftDesktop">
-      <CircularButton
-        handleClick={() => router.push(githubLink)}
-        icon={GithubLogo}
-      />
-      <CircularButton
-        handleClick={() => router.push(linkedinLink)}
-        icon={LinkedinLogo}
-      />
+      {socialMediaData.map((social, index) => (
+        <Link
+          href={social.link}
+          key={index}
+          target="_blank"
+          className="w-[30px] h-[30px] lg:w-[36px] lg:h-[36px] bg-[#1B1B1B] sm:hover:bg-dark-blue/[.20] border border-light-blue/50 rounded-full hover:drop-shadow-light cursor-pointer transition-all duration-200 flex justify-center items-center"
+        >
+          {social.icon()}
+        </Link>
+      ))}
     </div>
   )
 }
