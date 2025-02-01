cef978663c5af2f140883348548f020b7918986f
Bystrol <michalbystryk14@wp.pl>
2025-02-01 20:59:49 +0100

    style: adjust TechnologyBox padding

--- src/components/atoms/TechnologyBox.tsx
+++ src/components/atoms/TechnologyBox.tsx
@@ -1,7 +1,7 @@
 export default function TechnologyBox({ text }: { text: string }) {
   return (
-    <div className="px-[12px] sm:px-[15px] py-[8px] rounded-full bg-[#1B1B1B] border border-[#2960F8]/[0.5]">
-      <p className="text-[12px] sm:text-[14px] uppercase">{text}</p>
+    <div className="px-[12px] sm:px-[15px] py-[4px] sm:py-[6px] rounded-full bg-[#1B1B1B] border border-[#2960F8]/[0.5]">
+      <p className="text-[10px] sm:text-[14px] uppercase">{text}</p>
     </div>
   )
 }
