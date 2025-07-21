9d7fc8f50785abe188f04848008dc904b3fd4c0b
Bystrol <michalbystryk14@wp.pl>
2025-07-21 16:10:46 +0200

    feat: add sitemap support for internationalization

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.michalbystryk.dev',
+  alternateRefs: [
+    {
+      href: 'https://www.michalbystryk.dev',
+      hreflang: 'en'
+    },
+    {
+      href: 'https://www.michalbystryk.dev',
+      hreflang: 'pl'
+    }
+  ],
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [
